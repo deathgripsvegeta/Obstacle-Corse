646b8079eab5aa70d85339645405b987bc25810f
deathgripsvegeta <sophiahowell831@gmail.com>
2023-03-31 11:31:34 -0700

    trying to work on health thing

--- 3D Obstacle Corse/Assets/Scripts/Score.cs
+++ 3D Obstacle Corse/Assets/Scripts/Score.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Score : MonoBehaviour
-{
+{      
+    public TextMeshProUGUI health;
+    public GameObject DeathPanel;
+    [SerializeField] public float healthPoints = 5;
     [SerializeField] private int _hits = 0;
 
     private void OnCollisionEnter(Collision other)
@@ -11,6 +16,14 @@ public class Score : MonoBehaviour
         if(other.gameObject.tag != "Hit")
         {
             _hits++;
+            
         }
+        if(healthPoints < _hits)
+        {
+            DeathPanel.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+        
     }
+
 }
